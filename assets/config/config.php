cf8800b966a25477c456725a81a616a5806c7e62
Ng Qi Teng <63818402+teng-1206@users.noreply.github.com>
2023-10-15 21:40:18 +0800

    Update config.php

--- assets/config/config.php
+++ assets/config/config.php
@@ -51,34 +51,34 @@
         or define( 'ASSETS_PATH', realpath( dirname( __FILE__ ) . '//..//' ) );
         
     defined( 'API_PATH' )
-        or define( 'API_PATH', ASSETS_PATH . '//api' );
+        or define( 'API_PATH', ASSETS_PATH . '//api//' );
 
     defined( 'CONFIG_PATH' )
-        or define( 'CONFIG_PATH', ASSETS_PATH . '//config' );
+        or define( 'CONFIG_PATH', ASSETS_PATH . '//config//' );
 
     defined( 'CSS_PATH' )
-        or define( 'CSS_PATH', ASSETS_PATH . '//css' );
+        or define( 'CSS_PATH', ASSETS_PATH . '//css//' );
 
     defined( 'DOCS_PATH' )
-        or define( 'DOCS_PATH', ASSETS_PATH . '//docs' );
+        or define( 'DOCS_PATH', ASSETS_PATH . '//docs//' );
         
     defined( 'IMG_PATH' )
-        or define( 'IMG_PATH', ASSETS_PATH . '//img' );
+        or define( 'IMG_PATH', ASSETS_PATH . '//img//' );
 
     defined( 'JS_PATH' )
-        or define( 'JS_PATH', ASSETS_PATH . '//js' );
+        or define( 'JS_PATH', ASSETS_PATH . '//js//' );
 
     defined( 'MODULES_PATH' )
-        or define( 'MODULES_PATH', ASSETS_PATH . '//modules' );
+        or define( 'MODULES_PATH', ASSETS_PATH . '//modules//' );
 
     defined( 'PLUGINS_PATH' )
-        or define( 'PLUGINS_PATH', ASSETS_PATH . '//plugins' );   
+        or define( 'PLUGINS_PATH', ASSETS_PATH . '//plugins//' );
     
     defined( 'TEMPLATES_PATH' )
-        or define( 'TEMPLATES_PATH', ASSETS_PATH . '//templates' );
+        or define( 'TEMPLATES_PATH', ASSETS_PATH . '//templates//' );
 
     defined( 'UPLOADS_PATH' )
-        or define( 'UPLOADS_PATH', ASSETS_PATH . '//uploads' );
+        or define( 'UPLOADS_PATH', ASSETS_PATH . '//uploads//' );
 
     session_start();
 ?>
